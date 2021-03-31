4bc40b082affe6469401b374dbb853c258e318d4
Patrick Perron <webpape@gmail.com>
2021-03-31 15:17:36 -0400

    Initialise project

---
+++ src/javascript/index.js
@@ -0,0 +1,10 @@
+// Vous devez récupérer la liste des pokémons et en afficher 10 dans le tableau #pokemon-list en incluant leurs abilités et leurs types.
+// Ensuite vous devez mettre un bouton pour afficher les détails d'un pokémon, je veux au moins 3 détails additionnels,
+// Vous devez pouvoir fermer les détails et retourner à la liste
+// Vous devez mettre un état de loading avant le chargement
+// Vous avez le droit d'importer n'importe quel library (jQuery, axios, etc... ) et Bootstrap 5 est déjà loader
+// Vous devez créer des fonctions ou class à partir d'ici
+
+// Appel vers l'API PokéAPI
+// Pour avec la liste des pokémons : https://pokeapi.co/api/v2/pokemon/?limit=10
+// pour les détails d'un pokémon : https://pokeapi.co/api/v2/pokemon/{id}
